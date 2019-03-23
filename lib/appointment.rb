7345b40dd577224f1057edf73c879add7033c485
Katie Blizzard <kblizzard11@gmail.com>
2019-03-23 23:06:06 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,1 +1,17 @@
-class Ap
+class Appointment 
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient
+    @doctor = doctor
+    @@all << self 
+  end 
+  
+end 
+    
