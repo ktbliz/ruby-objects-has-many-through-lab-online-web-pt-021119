e4042daf28fee4a8e5114233b99054c81eb3e172
Katie Blizzard <kblizzard11@gmail.com>
2019-03-23 23:04:12 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,4 +16,12 @@ class Doctor
     Appointment.new(date, patient, self)
   end 
   
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self }
+  end 
+  
+  def patients
+    appointments.map { |appointment| appointment.patient }
+  end 
+  
 end 
